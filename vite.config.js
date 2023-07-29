02d58092d9b2f932751e9f86d1cad9a023781a9a
stephan <smuts1989@gmail.com>
2023-07-30 00:14:59 +0200

    Changed config file to make chunks

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { splitVendorChunkPlugin } from 'vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), splitVendorChunkPlugin()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          if (id.includes('node_modules')) {
+            // all node_modules are put into a "vendor" chunk
+            return 'vendor'
+          }
+        },
+      },
+    },
+  },
 })
