0c35574d9fdf2110b6fc209c35fd6751a3578ffd
stephan <smuts1989@gmail.com>
2023-07-30 17:03:06 +0200

    Changed chunking back

--- vite.config.js
+++ vite.config.js
@@ -2,20 +2,31 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { visualizer } from 'rollup-plugin-visualizer'
 
+// This is used to group dependencies by package
+function packageName(id) {
+  let match = id.match(/([^\/\\]+)\/?$/)
+  if (match) return match[1]
+  return id
+}
+
 export default defineConfig({
   plugins: [
     react(),
-    {
-      ...visualizer({
-        open: true,
-        gzipSize: true,
-        brotliSize: true,
-      }),
-      enforce: 'post',
-      apply: 'build',
-    },
+    visualizer({
+      open: true,
+      gzipSize: true,
+      brotliSize: true
+    })
   ],
   build: {
-    chunkSizeWarningLimit: 750, // size in KiB
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          if (id.includes('node_modules')) {
+            return packageName(id)
+          }
+        },
+      },
+    },
   },
 })
